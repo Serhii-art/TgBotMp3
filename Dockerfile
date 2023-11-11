1f5d41ce9522244116adbfe74f2bf2a5c8089fe0
Serhii-art <serhii.allakov1@icloud.com>
2023-11-11 20:21:50 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:16-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+ENV PORT=3000
+
+CMD ["npm", "start"]
+
+
+
+
+# Устанавливаем зависимости
+# RUN ci
+
---
+++ src/openai.js
@@ -0,0 +1,43 @@
+import OpenAI from "openai";
+import config from "config";
+import { createReadStream } from "fs";
+
+class OpenIA {
+  roles = {
+    ASSISTANT: "assistant",
+    USER: "user",
+    SYSTEM: "system",
+  };
+  constructor(apiKey) {
+    this.openai = new OpenAI({
+      apiKey, // Используем переданный API-ключ
+    });
+  }
+
+  async chat(messages) {
+    try {
+      const response = await this.openai.chat.completions.create({
+        messages,
+        model: "gpt-3.5-turbo",
+      });
+      return response.choices[0].message;
+    } catch (e) {
+      console.log("Error while gpt Chat: ");
+    }
+  }
+
+  async transcription(filepath) {
+    try {
+      const response = await this.openai.audio.transcriptions.create({
+        model: "whisper-1",
+        file: createReadStream(filepath), // Передаем поток файла, созданный из пути к файлу
+      });
+      return response.text;
+      //   return response.data.text;
+    } catch (e) {
+      console.log("Error while transcriptions: ", e.message);
+    }
+  }
+}
+
+export const openai = new OpenIA(config.get("OPENAI_KEY"));
